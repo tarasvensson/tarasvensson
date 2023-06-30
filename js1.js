ba99e6b3835a3b61a4cd97ebc1575103e76f68c8
tarasvensson <137794750+tarasvensson@users.noreply.github.com>
2023-06-30 10:40:35 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,27 @@
+
+var video = document.getElementById("videoLogo");
+video.addEventListener("canplay", function() {
+  setTimeout(function() {
+    video.play();
+  }, 600);
+
+});
+
+window.onload = function() {
+  window.setTimeout(fadeout, 8000); //8 seconds
+}
+
+function fadeout() {
+  document.getElementById('fadeout').style.opacity = '1';
+};
+
+$(document).ready(function() {
+	
+	$('.btn').delay(400).queue(function(next) {
+		$(this).addClass('hover').delay(1800).queue(function(next) {
+			$(this).removeClass('hover');
+		});
+		next();
+	});
+});
+
